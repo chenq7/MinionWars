7c59eab86fe3d08f29617421e47753d5cab7d324
lawrence_nic <your_email_here>
2020-02-26 16:30:12 -0500

    add images

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -12,7 +12,7 @@ class Splash extends React.Component {
 
     render(){
         return (
-          <div>
+          <div className='splash-page'>
               <Nav/>
         <section id="shop">
             <div className="content">
