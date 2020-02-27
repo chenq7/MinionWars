c8db9389601dbc49c060cc6af944066751f7cffb
John Jiang <csjohnjj@gmail.com>
2020-02-27 13:43:28 -0500

    update market

--- frontend/src/components/cardsIndex/card_index.js
+++ frontend/src/components/cardsIndex/card_index.js
@@ -43,7 +43,7 @@ class CardIndex extends React.Component {
     
     return (
       <div>
-        <h1>Card Index Page</h1>  
+        <h1>Minions Market</h1>  
         <span className="user-profile-btn" onClick={() => this.props.history.push(`/users/${this.props.currentUser.id}`)}>user profile</span>
         <div className='cards-list'>
           {minions}
