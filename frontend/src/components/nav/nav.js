a2bb463cdceb7e29f33e39c9ea07cdfe0a1dfb59
lawrence_nic <your_email_here>
2020-03-12 16:50:38 -0400

    refactor css splash and nav

--- frontend/src/components/nav/nav.js
+++ frontend/src/components/nav/nav.js
@@ -1,48 +1,51 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import "./nav.css";
-import Logo from '../../../src/app/assets/logo.png'
-
-
+import Logo from "../../../src/app/assets/logo.png";
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.logoutUser = this.logoutUser.bind(this);
     this.getLinks = this.getLinks.bind(this);
-    this.changeClass = this.changeClass.bind(this)
+    this.changeClass = this.changeClass.bind(this);
 
     this.state = {
       hidden: true
-    }
-    
+    };
   }
 
   logoutUser(e) {
     e.preventDefault();
     this.props.logout();
-    this.props.history.push('/');
+    this.props.history.push("/");
   }
 
-  changeClass(){
+  changeClass() {
     const currentState = this.state.hidden;
 
-    this.setState({hidden: !currentState})
+    this.setState({ hidden: !currentState });
   }
 
   getLinks() {
     if (this.props.loggedIn) {
       return (
-        <div className='nav-items'>
-          <div className="userProfile" onClick={() => this.props.history.push(`/users/${this.props.currentUser.id}`)}>Profile</div>
+        <div className="nav-items">
+          <div
+            className="userProfile"
+            onClick={() =>
+              this.props.history.push(`/users/${this.props.currentUser.id}`)
+            }
+          >
+            Profile
+          </div>
           <div className="SessionButton" onClick={this.logoutUser}>
             <a>Logout</a>
           </div>
-        </div >
+        </div>
       );
     } else {
       return (
-
         <div className="SessionButton" onClick={this.props.loginForm}>
           <a>Login</a>
         </div>
@@ -50,39 +53,40 @@ class Nav extends React.Component {
     }
   }
 
-  showGetLinks(){
-    if(this.props.loggedIn){
-
+  showGetLinks() {
+    if (this.props.loggedIn) {
     }
   }
 
   render() {
     return (
-      <div className="background">
-        <header>
-          <div className="logo">
-            <img className="logo" src={Logo} />
-          </div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/minions">Minions</Link>
-            </li>
-          </ul>
-
-            <div className="dropdown">
-              <i onClick={this.changeClass} class="fas fa-bars dots"></i>
-              <div className={this.state.hidden ? "hide-me" : "dropdown-content"}>
-                <a href="#">
-                  <i className="fab fa-github"></i>
-                </a>
-                <li>{this.getLinks()}</li>
+      <header>
+        <div className="logo-container">
+          <img className="logo" src={Logo} />
+        </div>
+        <ul>
+          
+          
+        </ul>
+        <div className='dropdown-container'>
+          <i onClick={this.changeClass} class="fas fa-bars hamburger"></i>
+          <div className="dropdown">
+              <div
+                className={this.state.hidden ? "hide-me" : "dropdown-content"}
+              >
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/minions">Marketplace</Link>
+                  </li>
+                  <li>{this.getLinks()}</li>
+                </ul>
               </div>
-            </div>
-        </header>
-      </div>
+          </div>
+        </div>
+      </header>
     );
   }
 }
