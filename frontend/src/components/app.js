3e95dea2bca6d7c0683e01bb959f366311b1d0a9
John Jiang <csjohnjj@gmail.com>
2020-03-19 15:24:50 -0400

    update about us/ remove unused

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -9,6 +9,7 @@ import Nav from "./nav/nav_container";
 import CardIndex from './cardsIndex/card_index_container'; //we need to change this
 import UserProfile from './users/user_profile_container';
 import HowToPlay from './how_to_play/how_to_play';
+import AboutUs from './about_us/about_us';
 import Versus from "./users/versus";
 
 const App = () => (
@@ -17,6 +18,7 @@ const App = () => (
     <Nav />
 
     <Switch>
+      <Route exact path="/about" component={AboutUs} />
       <Route exact path='/learn' component={HowToPlay}/>
       <Route exact path="/" component={SplashContainer} />
       <Route exact path="/minions" component={CardIndex} />
--- frontend/src/components/how_to_play/how_to_play.jsx
+++ frontend/src/components/how_to_play/how_to_play.jsx
@@ -7,9 +7,7 @@ import M2 from '../../app/assets/witch.png';
 
 
 class HowToPlay extends React.Component {
-
-
-
+    
     render(){
         return(
             <div className='how-to-container'>
